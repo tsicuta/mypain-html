52fd65294328b4a19bf5ea3174991676f63e427b
tsicuta <94219192+tsicuta@users.noreply.github.com>
2022-12-18 23:03:54 +0300

    Add files via upload

--- Lesson 8/server.php
+++ Lesson 8/server.php
@@ -1,52 +1,32 @@
-<?php
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
-
-$action = $_REQUEST['action'];
+<?php 
+session_start();
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "dannie";
+$dbname = "2075";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Проверяем соединение
+// Check connection
 if ($conn->connect_error) {
-    die("Ошибка подключения: " . $conn->connect_error);
-  }
-  echo "Connected successfully";
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
-if($action==='reg'){
-    $sql = "SELECT * FROM dannie where email='". $data['email']."'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0){
-        http_response_code(503);
-        echo "ERROR: user already exist";
-    } else{
-        $sql = "INSERT INTO dannie (name, surname, email, password)
-        VALUES('".$data['name']."', '".$data['surname']."', '".$data['email']."', '".$data['password']."')"
-    
-    if($conn->query($sql)===TRUE){
-        echo "new record created sucessfully";
-    } else{
-        echo "ERROR: ".$sql."<br>".$conn->error;
-    }
-}
-}
-else if($action === 'login'){
-    $data = [
-        'email' => $_REQUEST['login'],
-        'password' => $_REQUEST['password']
-    ];
-    $sql = "SELECT * FROM dannie where email='". $data['email']."' AND password='". $data['password']."'";
 
-$result = $conn->query($sql);
-if ($result->num_rows > 0){
-    echo "SUCCESS";
-} else{
-    echo 'ERROR: user not found';
+$data=json_decode(file_get_contents("php://input"), true);
+
+$sql = "INSERT INTO users (username,s_name,usermail,passrwrd)
+VALUES ('" . $data['name'] . "', '" . $data['surname'] . "', '" . $data['email'] . "', '" . $data['password'] . "')";
+/*VALUES ('John', 'Doe', 'john@example.com', '1235', 'male')";
+*/
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
-}
-  ?>
-  
+
+$conn->close();
+?>
